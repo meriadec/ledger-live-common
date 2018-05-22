96c8c2ef584ed8de6747fd6faa7915f8af6f0a73
meriadec <meriadec.pillet@gmail.com>
2018-05-22 18:51:34 +0200

    WIP

--- src/types/account.js
+++ src/types/account.js
@@ -6,8 +6,8 @@ import type { OperationRaw, Operation } from "./operation";
 
 export type Address = {
   str: string,
-  path: string,
-}
+  path: string
+};
 
 export type AccountRaw = {
   // unique account identifier
@@ -19,20 +19,15 @@ export type AccountRaw = {
   // account path on the device
   path: string,
 
-  // account wallet path on the device
-  walletPath: string,
+  // next receive address
+  freshAddress: string,
+  freshAddressPath: string,
 
   // account name
   name: string,
 
   // whether or not the account is segwit (useful to not re-parse path everytime)
   isSegwit: boolean,
-
-  // account bitcoinAddress received when derivating account path
-  address: string,
-
-  // bunch of fresh receive addresses, calculated by libcore
-  addresses: Address[],
 
   // account balance in satoshi
   balance: number,
@@ -42,10 +37,6 @@ export type AccountRaw = {
 
   // whether or not the account is archived
   archived: boolean,
-
-  // Actually used in the desktop app
-  // TODO: should get rid of that if possible
-  index: number,
 
   // ------------------------------------- Specific raw fields
 
@@ -72,20 +63,15 @@ export type Account = {
   // account path on the device
   path: string,
 
-  // account wallet path on the device
-  walletPath: string,
+  // next receive address
+  freshAddress: string,
+  freshAddressPath: string,
 
   // account name
   name: string,
 
   // whether or not the account is segwit (useful to not re-parse path everytime)
   isSegwit: boolean,
-
-  // account bitcoinAddress received when derivating account path
-  address: string,
-
-  // transaction all addresses (actually needed by desktop app)
-  addresses: Address[],
 
   // account balance in satoshi
   balance: number,
@@ -95,10 +81,6 @@ export type Account = {
 
   // whether or not the account is archived
   archived: boolean,
-
-  // Actually used in the desktop app
-  // TODO: should get rid of that if possible
-  index: number,
 
   // ------------------------------------- Specific account fields
 
